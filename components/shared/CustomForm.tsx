efa5c1b63510c9b7dbf5abd3b0b0cde6d8efe971
Antarcticpx <antarcticpx@gmail.com>
2025-08-27 05:07:53 +0700

    feat : create customable input component

---
+++ components/shared/CustomForm.tsx
@@ -0,0 +1,40 @@
+import { Control } from "react-hook-form"
+import { FormField, FormItem } from "../ui/form"
+import RenderField from "./RenderField"
+
+export enum FieldType {
+    INPUT = 'input',
+    SELECT = 'select',
+    FILE = 'file',
+    TEXTAREA = 'textarea',
+    NUMBER = 'number',
+    UPLOAD = 'upload'
+}
+
+export interface CustomFormProps {
+    control: Control<any>
+    type: FieldType
+    name: string
+    label?: string
+    placeholder?: string
+    value?: string
+    children?: React.ReactNode
+    disabled?: boolean
+}
+
+
+export default function CustomForm(props: CustomFormProps) {
+    const { name, control, value } = props
+  return (
+    <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+        <FormItem>
+            
+            <RenderField field={field} props={props}/>
+        </FormItem>
+        )}
+    />
+  )
+}
---
+++ components/shared/RenderField.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { CustomFormProps, FieldType } from './CustomForm'
+import { FormControl, FormLabel, FormMessage } from '../ui/form'
+import { Input } from '../ui/input'
+import { Select, SelectContent, SelectTrigger, SelectValue } from '../ui/select'
+import { Textarea } from '../ui/textarea'
+
+export default function RenderField({ field, props }: { field: any, props: CustomFormProps }) {
+    switch (props.type) {
+        case FieldType.INPUT:
+            return (
+                <>
+                    <FormLabel>{props.label}</FormLabel>
+                    <FormControl>
+                        <Input 
+                            placeholder={props.placeholder}
+                            className="transition-all duration-300 border-gray-200 focus:border-blue-400 focus:ring-blue-400/20"
+                            {...field}
+                           
+                        />
+                    </FormControl>
+                    <FormMessage className="text-sm text-red-500"/>
+                </>
+            )
+        case FieldType.NUMBER:
+            return (
+                <>
+                    <FormLabel>{props.label}</FormLabel>
+                    <FormControl>
+                        <Input 
+                            placeholder={props.placeholder} {...field}
+                            className="transition-all duration-300 border-gray-200 focus:border-blue-400 focus:ring-blue-400/20"
+                            type="number"
+                        />
+                    </FormControl>
+                    <FormMessage className="text-sm text-red-500" />
+                </>
+            )
+        case FieldType.TEXTAREA:
+            return (
+                <>
+                    
+                    <FormLabel>{props.label}</FormLabel>
+                    <FormControl>
+                        <Textarea className="transition-all duration-300 border-gray-200 focus:border-blue-400 focus:ring-blue-400/20 h-60" placeholder={props.placeholder} {...field}/>
+                    </FormControl>
+                    <FormMessage className="text-sm text-red-500" />
+                
+                </>
+                
+            )
+
+        case FieldType.SELECT:
+            return (
+                <>
+                    <FormLabel>{props.label}</FormLabel>
+                    <FormControl>
+                        <Select defaultValue={field.value} onValueChange={field.onChange}>
+                            <SelectTrigger className="transition-all duration-300 border-gray-200 focus:border-blue-400 focus:ring-blue-400/20 w-full">
+                                <SelectValue placeholder={props.placeholder} />
+                            </SelectTrigger>
+                            <SelectContent className="bg-white shad-select-content">
+                                {props.children}
+                            </SelectContent>
+                        </Select>
+
+                    </FormControl>
+                    <FormMessage className="text-sm text-red-500" />
+                </>
+            )
+        case FieldType.UPLOAD:
+            return (
+                <>
+                    
+                    <FormControl>
+                        <Input id="picture" type="file"  placeholder={props.placeholder} {...field} className="shad-input bg-zinc-900 border-none w-full" />
+                        
+                    </FormControl>
+                    <FormMessage className="text-sm text-red-500" />
+                    
+                </>
+            )
+    
+        default:
+            break;
+    }
+}
