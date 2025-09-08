9681a6b04a727cbd90e5e22effe3ca7e61432a77
Antarcticpx <antarcticpx@gmail.com>
2025-09-08 17:30:06 +0700

    feat : created update feature for products

--- app/(auth)/dashboard/product/[id]/edit/page.tsx
+++ app/(auth)/dashboard/product/[id]/edit/page.tsx
@@ -1,12 +1,11 @@
-
-
-import Steps from "@/components/shared/Steps";
-import { Button } from "@/components/ui/button";
+import { getProductById } from "@/lib/action/product.action"
+import Steps from '@/components/shared/Steps';
 
 
 
-export default function page() {
-    
+export default async function page({ params }: { params: Promise<{id: string}>}) {
+    const { id } = await params
+    const product = await getProductById(id)
   return (
     <section className="h-screen w-full px-8 pt-16 md:pt-0 md:-mt-14">
         <div className="flex justify-between ">
@@ -14,10 +13,10 @@ export default function page() {
                 <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">Add New Product</h1>
                 <span className="text-slate-600 mt-1">Create your product listing in just a few steps</span>
             </div>
-           
+            
         </div>
 
-        <Steps type="create" />
+        <Steps type="update" dataEdit={product} />
     </section>
   )
 }
